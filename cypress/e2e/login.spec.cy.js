24c952075b69d4eba1f6a5b9291fe9891026de50
Silvio Barbosa <siilvio.barbosa@gmail.com>
2025-04-16 02:32:45 -0400

    Add Select list

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,8 +1,17 @@
 describe('Orange Test', () => {
+
+  const selectorsList = {
+    usernameField: "[name='username']",
+    passwordField: "[name='password']",
+    loginButton: "('.oxd-button')"
+  }
+
+
+
   it('login-success', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get('[name="username"]').type('Admin')
-    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
+    cy.get(selectorsList.usernameField).type('Admin')
+    cy.get(selectorsList.passwordField).type('admin123')
     cy.get('.oxd-button').click()
     cy.location('pathname').should('equal','/web/index.php/dashboard/index')
     cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').contains('Dashboard')
@@ -10,8 +19,8 @@ describe('Orange Test', () => {
 
   it('login-fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('ssss')
-    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('ssssaaaasasa')
+    cy.get(selectorsList.usernameField).type('ssss')
+    cy.get(selectorsList.passwordField).type('ssssaaaasasa')
     cy.get('.oxd-button').click()
     cy.get('.oxd-alert-content')
    })
